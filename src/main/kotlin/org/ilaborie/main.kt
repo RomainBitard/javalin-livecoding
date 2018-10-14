c22f5165748811ff39bdaf463e679bc380261478
Igor Laborie <ilaborie@gmail.com>
2018-10-14 17:50:35 +0200

    routing

--- src/main/kotlin/org/ilaborie/main.kt
+++ src/main/kotlin/org/ilaborie/main.kt
@@ -1,6 +1,11 @@
 package org.ilaborie
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
+import io.javalin.apibuilder.ApiBuilder.post
+import io.javalin.json.JavalinJackson
 
 fun main(args: Array<String>) {
     println("Hello, Toulouse, Jug")
@@ -8,11 +13,27 @@ fun main(args: Array<String>) {
     // TODO Javalin Hello World
     // @see https://javalin.io/documentation
 
+    JavalinJackson.configure(jacksonObjectMapper())
+
     Javalin.create()
         // configuration
         .disableStartupBanner()
         .enableCorsForAllOrigins() // 😱 FIXME
         .enableDebugLogging()
-        .get("/") { it.result("Hello, Toulouse JUG")}
+        // routing
+        .routes {
+            path("/api/emoji") {
+                get("/") { it.json(EmojiRepository.findAll()) }
+                get(":name") { ctx ->
+                    val name = ctx.pathParam("name")
+                    val result = EmojiRepository.findByName(name)
+                    ctx.json(result)
+                }
+                post("/") { ctx ->
+                    val emoji = ctx.body<Emoji>()
+                    ctx.json(EmojiRepository.insert(emoji))
+                }
+            }
+        }
         .start(8080)
 }
